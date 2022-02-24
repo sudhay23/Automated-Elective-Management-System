680deea7146b0ea330089e14c2477d9af495ec84
sudhay23 <sudhay2001@gmail.com>
2022-02-24 14:51:58 +0530

    Hide scrollbar when modal open + Prerequisite courses on hover

--- frontend/src/components/faculty/dashboard/AddCourseModal/index.js
+++ frontend/src/components/faculty/dashboard/AddCourseModal/index.js
@@ -3,132 +3,144 @@ import { FaArrowLeft } from "react-icons/fa";
 import { useState, useRef } from "react";
 
 const AddCourseModal = (props) => {
-	const [courseCode, setCourseCode] = useState("");
-	const [courseName, setCourseName] = useState("");
-	const [department, setDepartment] = useState("");
-	const [addedBy, setAddedBy] = useState("");
-	const [minCap, setMinCap] = useState("");
-	const [maxCap, setMaxCap] = useState("");
-	const [credits, setCredits] = useState("");
-	const formRef = useRef();
+    const [courseCode, setCourseCode] = useState("");
+    const [courseName, setCourseName] = useState("");
+    const [department, setDepartment] = useState("");
+    const [addedBy, setAddedBy] = useState("");
+    const [minCap, setMinCap] = useState("");
+    const [maxCap, setMaxCap] = useState("");
+    const [credits, setCredits] = useState("");
+    const [preRequisites, setPreRequisites] = useState("");
+    const formRef = useRef();
 
-	return (
-		<div className={styles.container}>
-			<div className={styles.addCourseForm}>
-				<div className={styles.formHeader}>
-					<FaArrowLeft
-						style={{ cursor: "pointer", fontSize: 28 }}
-						onClick={() => props.setShowAddCourse((x) => !x)}
-					/>
-				</div>
-				<form action="#" className={styles.formContent} ref={formRef}>
-					<input
-						required
-						type="text"
-						className={styles.inputField}
-						value={courseCode}
-						placeholder="Enter Course Code"
-						onChange={(e) => {
-							setCourseCode(e.target.value);
-						}}
-					/>
-					<input
-						required
-						type="text"
-						className={styles.inputField}
-						value={courseName}
-						placeholder="Enter Course Name"
-						onChange={(e) => {
-							setCourseName(e.target.value);
-						}}
-					/>
-					<input
-						required
-						type="text"
-						className={styles.inputField}
-						value={department}
-						placeholder="Enter Department"
-						onChange={(e) => {
-							setDepartment(e.target.value);
-						}}
-					/>
-					<input
-						required
-						type="text"
-						className={styles.inputField}
-						value={addedBy}
-						placeholder="Added By (full name to display)"
-						onChange={(e) => {
-							setAddedBy(e.target.value);
-						}}
-					/>
-					<input
-						required
-						type="number"
-						className={styles.inputField}
-						value={minCap}
-						placeholder="Enter Minimum Cap"
-						onChange={(e) => {
-							setMinCap(e.target.value);
-						}}
-					/>
-					<input
-						required
-						type="number"
-						className={styles.inputField}
-						value={maxCap}
-						placeholder="Enter Maximum Cap"
-						onChange={(e) => {
-							setMaxCap(e.target.value);
-						}}
-					/>
-					<input
-						required
-						type="number"
-						className={styles.inputField}
-						value={credits}
-						placeholder="Enter Credits"
-						onChange={(e) => {
-							setCredits(e.target.value);
-						}}
-					/>
-					<input
-						type="submit"
-						value="Submit"
-						className={styles.submitBtn}
-						onClick={(e) => {
-							// TODO: Add course on DB
-							e.preventDefault();
-							if (formRef.current.reportValidity()) {
-								const newCourse = {
-									id: (
-										parseInt(
-											props.courses[
-												props.courses.length - 1
-											].id
-										) + 1
-									).toString(),
-									courseCode,
-									courseName,
-									department,
-									addedBy,
-									addedOn: new Date().toLocaleDateString(),
-									minCap,
-									maxCap,
-									credits,
-								};
-								props.setCourses((courses) => [
-									...courses,
-									newCourse,
-								]);
-								props.setShowAddCourse(false);
-							}
-						}}
-					/>
-				</form>
-			</div>
-		</div>
-	);
+    return (
+        <div className={styles.container}>
+            <div className={styles.addCourseForm}>
+                <div className={styles.formHeader}>
+                    <FaArrowLeft
+                        style={{ cursor: "pointer", fontSize: 28 }}
+                        onClick={() => props.setShowAddCourse((x) => !x)}
+                    />
+                </div>
+                <form action="#" className={styles.formContent} ref={formRef}>
+                    <input
+                        required
+                        type="text"
+                        className={styles.inputField}
+                        value={courseCode}
+                        placeholder="Enter Course Code"
+                        onChange={(e) => {
+                            setCourseCode(e.target.value);
+                        }}
+                    />
+                    <input
+                        required
+                        type="text"
+                        className={styles.inputField}
+                        value={courseName}
+                        placeholder="Enter Course Name"
+                        onChange={(e) => {
+                            setCourseName(e.target.value);
+                        }}
+                    />
+                    <input
+                        required
+                        type="text"
+                        className={styles.inputField}
+                        value={department}
+                        placeholder="Enter Department"
+                        onChange={(e) => {
+                            setDepartment(e.target.value);
+                        }}
+                    />
+                    <input
+                        required
+                        type="text"
+                        className={styles.inputField}
+                        value={addedBy}
+                        placeholder="Added By (full name to display)"
+                        onChange={(e) => {
+                            setAddedBy(e.target.value);
+                        }}
+                    />
+                    <input
+                        required
+                        type="number"
+                        className={styles.inputField}
+                        value={minCap}
+                        placeholder="Enter Minimum Cap"
+                        onChange={(e) => {
+                            setMinCap(e.target.value);
+                        }}
+                    />
+                    <input
+                        required
+                        type="number"
+                        className={styles.inputField}
+                        value={maxCap}
+                        placeholder="Enter Maximum Cap"
+                        onChange={(e) => {
+                            setMaxCap(e.target.value);
+                        }}
+                    />
+                    <input
+                        required
+                        type="number"
+                        className={styles.inputField}
+                        value={credits}
+                        placeholder="Enter Credits"
+                        onChange={(e) => {
+                            setCredits(e.target.value);
+                        }}
+                    />
+                    <input
+                        required
+                        type="text"
+                        className={styles.inputField}
+                        value={preRequisites}
+                        placeholder="Enter Prerequisite Course Codes (comma seperated)"
+                        onChange={(e) => {
+                            setPreRequisites(e.target.value);
+                        }}
+                    />
+                    <input
+                        type="submit"
+                        value="Submit"
+                        className={styles.submitBtn}
+                        onClick={(e) => {
+                            // TODO: Add course on DB
+                            e.preventDefault();
+                            if (formRef.current.reportValidity()) {
+                                const newCourse = {
+                                    id: (
+                                        parseInt(
+                                            props.courses[
+                                                props.courses.length - 1
+                                            ].id
+                                        ) + 1
+                                    ).toString(),
+                                    courseCode,
+                                    courseName,
+                                    department,
+                                    addedBy,
+                                    addedOn: new Date().toLocaleDateString(),
+                                    minCap,
+                                    maxCap,
+                                    credits,
+                                    preRequisites,
+                                };
+                                props.setCourses((courses) => [
+                                    ...courses,
+                                    newCourse,
+                                ]);
+                                props.setShowAddCourse(false);
+                            }
+                        }}
+                    />
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default AddCourseModal;
--- frontend/src/components/faculty/dashboard/CourseTable/index.js
+++ frontend/src/components/faculty/dashboard/CourseTable/index.js
@@ -3,76 +3,79 @@ import { FaEdit, FaTrash } from "react-icons/fa";
 import { useState } from "react";
 
 const CourseTable = (props) => {
-	return (
-		<div className={styles.container}>
-			<table cellSpacing={0}>
-				<thead>
-					<tr>
-						<th className={styles.snoCell}>S.no</th>
-						<th className={styles.courseCodeCell}>Course Code</th>
-						<th className={styles.courseNameCell}>Course Name</th>
-						<th className={styles.departmentCell}>Department</th>
-						<th className={styles.addedByCell}>Added By</th>
-						<th className={styles.addedOnCell}>Added On</th>
-						<th className={styles.minCapCell}>Min.Cap</th>
-						<th className={styles.maxCapCell}>Max.Cap</th>
-						<th className={styles.actionsCell}>Actions</th>
-					</tr>
-				</thead>
-				<tbody>
-					{props.courses.map((course, idx) => (
-						<tr key={idx}>
-							<td className={styles.snoCellData}>{idx + 1}</td>
-							<td className={styles.courseCodeCellData}>
-								{course.courseCode}
-							</td>
-							<td className={styles.courseNameCellData}>
-								{course.courseName}
-								<br /> (Credits: {course.credits})
-							</td>
-							<td className={styles.departmentCellData}>
-								{course.department}
-							</td>
-							<td className={styles.addedByCellData}>
-								{course.addedBy}
-							</td>
-							<td className={styles.addedOnCellData}>
-								{course.addedOn}
-							</td>
-							<td className={styles.minCapCellData}>
-								{course.minCap}
-							</td>
-							<td className={styles.maxCapCellData}>
-								{course.maxCap}
-							</td>
-							<td className={styles.actionsCellData}>
-								<FaEdit
-									style={{
-										marginRight: 10,
-										fontSize: "20px",
-										cursor: "pointer",
-									}}
-								/>
-								<FaTrash
-									style={{
-										marginLeft: 10,
-										fontSize: "20px",
-										cursor: "pointer",
-									}}
-									onClick={() => {
-										//TODO: Should trigger a DELETE request for DB too
-										props.setCourses((x) =>
-											x.filter((y) => y.id != course.id)
-										);
-									}}
-								/>
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
-	);
+    return (
+        <div className={styles.container}>
+            <table cellSpacing={0}>
+                <thead>
+                    <tr>
+                        <th className={styles.snoCell}>S.no</th>
+                        <th className={styles.courseCodeCell}>Course Code</th>
+                        <th className={styles.courseNameCell}>Course Name</th>
+                        <th className={styles.departmentCell}>Department</th>
+                        <th className={styles.addedByCell}>Added By</th>
+                        <th className={styles.addedOnCell}>Added On</th>
+                        <th className={styles.minCapCell}>Min.Cap</th>
+                        <th className={styles.maxCapCell}>Max.Cap</th>
+                        <th className={styles.actionsCell}>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.courses.map((course, idx) => (
+                        <tr
+                            key={idx}
+                            title={`Pre-requisites are ${course.preRequisites}`}
+                        >
+                            <td className={styles.snoCellData}>{idx + 1}</td>
+                            <td className={styles.courseCodeCellData}>
+                                {course.courseCode}
+                            </td>
+                            <td className={styles.courseNameCellData}>
+                                {course.courseName}
+                                <br /> (Credits: {course.credits})
+                            </td>
+                            <td className={styles.departmentCellData}>
+                                {course.department}
+                            </td>
+                            <td className={styles.addedByCellData}>
+                                {course.addedBy}
+                            </td>
+                            <td className={styles.addedOnCellData}>
+                                {course.addedOn}
+                            </td>
+                            <td className={styles.minCapCellData}>
+                                {course.minCap}
+                            </td>
+                            <td className={styles.maxCapCellData}>
+                                {course.maxCap}
+                            </td>
+                            <td className={styles.actionsCellData}>
+                                <FaEdit
+                                    style={{
+                                        marginRight: 10,
+                                        fontSize: "20px",
+                                        cursor: "pointer",
+                                    }}
+                                />
+                                <FaTrash
+                                    style={{
+                                        marginLeft: 10,
+                                        fontSize: "20px",
+                                        cursor: "pointer",
+                                    }}
+                                    onClick={() => {
+                                        //TODO: Should trigger a DELETE request for DB too
+                                        props.setCourses((x) =>
+                                            x.filter((y) => y.id != course.id)
+                                        );
+                                    }}
+                                />
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 export default CourseTable;
