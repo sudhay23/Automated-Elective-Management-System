68da1da2a00e5700debc1b0611cc6ec8c8492e60
sudhay23 <sudhay2001@gmail.com>
2022-02-25 12:05:55 +0530

    Added Role to user schema

--- backend/model/userRegistrationSchema.js
+++ backend/model/userRegistrationSchema.js
@@ -1,25 +1,30 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    min: 3,
-  },
-  email: {
-    type: String,
-    required: true,
-    min: 3,
-  },
-  password: {
-    type: String,
-    required: true,
-    max: 1024,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+    name: {
+        type: String,
+        required: true,
+        min: 3,
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 3,
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 1024,
+    },
+    role: {
+        type: String,
+        required: true,
+        min: 1,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 module.exports = mongoose.model("User", userSchema);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,61 +5,66 @@ const jwt = require("jsonwebtoken");
 
 // REGISTER route
 router.post("/register", async (req, res) => {
-  // check if user already exists
-  const userExists = await User.findOne({ email: req.body.email });
-  if (userExists)
-    return res.status(400).send("User already exists, please login");
+    // check if user already exists
+    const userExists = await User.findOne({ email: req.body.email });
+    if (userExists)
+        return res.status(400).send("User already exists, please login");
 
-  // hash the password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    // hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-  // else we create a new user and store it to database
+    // else we create a new user and store it to database
 
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: hashedPassword,
-  });
-  try {
-    const savedUser = await user.save();
-    res.send({
-      user: user._id,
-      name: user.name,
-      email: user.email,
-      password: user.password,
-      date: user.date,
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+        role: req.body.role,
     });
-  } catch (err) {
-    res.status(400).send(err);
-  }
+    try {
+        const savedUser = await user.save();
+        res.send({
+            user: user._id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            date: user.date,
+        });
+    } catch (err) {
+        res.status(400).send(err);
+    }
 });
 
 // LOGIN route
 router.post("/login", async (req, res) => {
-  // checking if email exists in database
-  const user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send("Email or password is incorrect");
-  const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword)
-    return res.status(400).send("Email or password is incorrect");
+    // checking if email exists in database
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("Email or password is incorrect");
+    const validPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+    );
+    if (!validPassword)
+        return res.status(400).send("Email or password is incorrect");
 
-  // create and assin a token to the user
-  const token = jwt.sign(
-    { _id: user._id, email: user.email, name: user.name },
-    process.env.JWT_TOKEN_SECRET,
-    {
-      expiresIn: "60s",
-    }
-  );
+    // create and assin a token to the user
+    const token = jwt.sign(
+        { _id: user._id, email: user.email, name: user.name, role: user.role },
+        process.env.JWT_TOKEN_SECRET,
+        {
+            expiresIn: "60s",
+        }
+    );
 
-  // send token in header and user data in body
-  res.header("auth-token", token).send({
-    user: user._id,
-    name: user.name,
-    email: user.email,
-    loginTime: user.date,
-  });
+    // send token in header and user data in body
+    res.header("auth-token", token).send({
+        user: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        loginTime: user.date,
+    });
 });
 
 module.exports = router;
