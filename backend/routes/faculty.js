0f9dbb5ea6d353f17667970d9b6a1c5d78cbd741
sudhay23 <sudhay2001@gmail.com>
2022-02-26 02:25:47 +0530

    Courses add and List routes done

--- backend/routes/faculty.js
+++ backend/routes/faculty.js
@@ -5,8 +5,37 @@ const verifyToken = require("./middleware/verifyToken");
 
 // Get List of Courses
 router.get("/courses", verifyToken, async (req, res) => {
-    const allCourses = await Course.find();
-    console.log(allCourses);
+    if (req.user.role == "faculty") {
+        const allCourses = await Course.find();
+        res.send(allCourses);
+    } else {
+        res.status(403).send("You are not authorized to do this");
+    }
+});
+
+// Add Course
+router.post("/courses", verifyToken, async (req, res) => {
+    if (req.user.role == "faculty") {
+        const newCourse = new Course({
+            courseCode: req.body.courseCode,
+            courseName: req.body.courseName,
+            department: req.body.department,
+            addedBy: req.body.addedBy,
+            minCap: req.body.minCap,
+            maxCap: req.body.maxCap,
+            credits: req.body.credits,
+            preRequisites: req.body.preRequisites,
+        });
+
+        try {
+            const savedCourse = await newCourse.save();
+            res.send(savedCourse);
+        } catch (error) {
+            res.status(400).send("Error Occurred");
+        }
+    } else {
+        res.status(403).send("You are not authorized to do this");
+    }
 });
 
 module.exports = router;
--- backend/routes/middleware/verifyToken.js
+++ backend/routes/middleware/verifyToken.js
@@ -1,15 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 function verifyToken(req, res, next) {
-    const token = req.cookie["auth-token"];
-    if (!token) res.status(401).send("Access denied. No token provided.");
-
     try {
-        const verified = jwt.verify(token, process.env.JWT_TOKEN_SECRET); // payload is returned
-        req.user = verified; // we are setting the user to the payload, it contains the user id and the iat
-        next();
-    } catch (err) {
-        res.status(400).send("Invalid token.");
+        const token = req.cookies["auth-token"];
+        if (!token) res.status(401).send("Access denied. No token provided.");
+
+        try {
+            const verified = jwt.verify(token, process.env.JWT_TOKEN_SECRET); // payload is returned
+            req.user = verified; // we are setting the user to the payload, it contains the user id and the iat
+            next();
+        } catch (err) {
+            res.status(400).send("Invalid token.");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("Token not found");
     }
 }
 
