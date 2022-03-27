fea43267ba6360af6071451f3e2b44b4bf1a0668
Sandeep Rajakrishnan <sandur43@gmail.com>
2022-03-27 19:50:58 +0530

    framer motion

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,26 @@
 import "../styles/globals.css";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+import { motion } from "framer-motion";
+
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <motion.div
+      key={router.route}
+      initial="initial"
+      animate="animate"
+      variants={{
+        initial: {
+          opacity: 0,
+        },
+        animate: {
+          opacity: 1,
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </motion.div>
+  );
 }
 
 export default MyApp;
