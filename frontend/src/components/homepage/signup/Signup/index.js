2390e927fa187f816b763ad1322992cc30db7ff0
sudhay23 <sudhay2001@gmail.com>
2022-03-27 20:52:10 +0530

    Changed Fetch request URLs to be dynamic

--- frontend/src/components/homepage/signup/Signup/index.js
+++ frontend/src/components/homepage/signup/Signup/index.js
@@ -4,146 +4,148 @@ import Link from "next/link";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const Signup = () => {
-  const [data, setData] = useState({
-    name: "",
-    role: "",
-    email: "",
-    password: "",
-  });
-  const [error, setError] = useState("");
-
-  const handleChange = ({ currentTarget: input }) => {
-    setData({ ...data, [input.name]: input.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log("handleSubmit");
-    fetch("http://localhost:4567/api/user/register", {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    }).then((res) => {
-      if (res.status == 409) {
-        toast.warn("Email already registered, sign in", {
-          position: "bottom-center",
-          autoClose: 2000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-        console.log("User already exists");
-        setError(res.error);
-      } else {
-        res.json();
-        console.log(res);
-        toast.success("🦄 Successfully Registered!", {
-          position: "top-center",
-          autoClose: 2000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-        setInterval(() => {
-          location.assign("/");
-        }, 2200);
-      }
+    const [data, setData] = useState({
+        name: "",
+        role: "",
+        email: "",
+        password: "",
     });
-  };
+    const [error, setError] = useState("");
 
-  return (
-    <div className={styles.signup_container}>
-      <div className={styles.signup_form_container}>
-        <div className={styles.left}>
-          <h1>Welcome Back</h1>
-          <Link href="/">
-            <button type="button" className={styles.white_btn}>
-              Sign in
-            </button>
-          </Link>
-        </div>
-        <div className={styles.right}>
-          <form
-            className={styles.form_container}
-            onSubmit={handleSubmit}
-            method="POST"
-          >
-            <h1>Create Account</h1>
-            <input
-              type="text"
-              placeholder="Name"
-              name="name"
-              onChange={handleChange}
-              value={data.name}
-              required
-              className={styles.input}
-            />
-            <input
-              type="text"
-              placeholder="role"
-              name="role"
-              onChange={handleChange}
-              value={data.role}
-              required
-              className={styles.input}
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              name="email"
-              onChange={handleChange}
-              value={data.email}
-              required
-              className={styles.input}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              onChange={handleChange}
-              value={data.password}
-              required
-              className={styles.input}
-            />
-            {error && <div className={styles.error_msg}>{error}</div>}
+    const handleChange = ({ currentTarget: input }) => {
+        setData({ ...data, [input.name]: input.value });
+    };
 
-            <button type="submit" className={styles.green_btn}>
-              Sign Up
-            </button>
-            <ToastContainer
-              position="bottom-center"
-              autoClose={2000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-            />
-            <ToastContainer
-              position="top-center"
-              autoClose={2000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-            />
-          </form>
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log("handleSubmit");
+        fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/user/register`, {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then((res) => {
+            if (res.status == 409) {
+                toast.warn("Email already registered, sign in", {
+                    position: "bottom-center",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+                console.log("User already exists");
+                setError(res.error);
+            } else {
+                res.json();
+                console.log(res);
+                toast.success("🦄 Successfully Registered!", {
+                    position: "top-center",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+                setInterval(() => {
+                    location.assign("/");
+                }, 2200);
+            }
+        });
+    };
+
+    return (
+        <div className={styles.signup_container}>
+            <div className={styles.signup_form_container}>
+                <div className={styles.left}>
+                    <h1>Welcome Back</h1>
+                    <Link href="/">
+                        <button type="button" className={styles.white_btn}>
+                            Sign in
+                        </button>
+                    </Link>
+                </div>
+                <div className={styles.right}>
+                    <form
+                        className={styles.form_container}
+                        onSubmit={handleSubmit}
+                        method="POST"
+                    >
+                        <h1>Create Account</h1>
+                        <input
+                            type="text"
+                            placeholder="Name"
+                            name="name"
+                            onChange={handleChange}
+                            value={data.name}
+                            required
+                            className={styles.input}
+                        />
+                        <input
+                            type="text"
+                            placeholder="role"
+                            name="role"
+                            onChange={handleChange}
+                            value={data.role}
+                            required
+                            className={styles.input}
+                        />
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            name="email"
+                            onChange={handleChange}
+                            value={data.email}
+                            required
+                            className={styles.input}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            onChange={handleChange}
+                            value={data.password}
+                            required
+                            className={styles.input}
+                        />
+                        {error && (
+                            <div className={styles.error_msg}>{error}</div>
+                        )}
+
+                        <button type="submit" className={styles.green_btn}>
+                            Sign Up
+                        </button>
+                        <ToastContainer
+                            position="bottom-center"
+                            autoClose={2000}
+                            hideProgressBar={false}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss
+                            draggable
+                            pauseOnHover
+                        />
+                        <ToastContainer
+                            position="top-center"
+                            autoClose={2000}
+                            hideProgressBar={false}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss
+                            draggable
+                            pauseOnHover
+                        />
+                    </form>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Signup;
