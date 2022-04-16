4a564204df47fb7682eca08872f5c21e2729d232
sudhay23 <sudhay2001@gmail.com>
2022-04-16 21:46:56 +0530

    Ineligible and Eligible Courses shown seperately with CGPA

--- frontend/pages/student/dashboard/index.js
+++ frontend/pages/student/dashboard/index.js
@@ -12,90 +12,91 @@ import styles from "../../../styles/faculty/dashboard/Home.module.css";
 import { useState, useEffect } from "react";
 
 export default function Home(props) {
-	const [loggedInStudent, setLoggedInStudent] = useState(null);
+    const [loggedInStudent, setLoggedInStudent] = useState(null);
 
-	// State variable to switch modal to add course and hold courses
-	const [courses, setCourses] = useState(props.courses);
+    // State variable to switch modal to add course and hold courses
+    const [courses, setCourses] = useState(props.courses);
 
-	// State variable to decide if AddCourse Form is in UPDATE mode
+    // State variable to decide if AddCourse Form is in UPDATE mode
 
-	useEffect(async () => {
-		const coursesRes = await fetch(
-			`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/student/courses`,
-			{ credentials: "include" }
-		);
-		// console.log(coursesRes);
-		if (coursesRes.status == 200) {
-			const courses = await coursesRes.json();
-			setCourses(courses);
-		} else {
-			setCourses([]);
-		}
-	}, []);
+    useEffect(async () => {
+        const coursesRes = await fetch(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/student/courses`,
+            { credentials: "include" }
+        );
+        // console.log(coursesRes);
+        if (coursesRes.status == 200) {
+            const courses = await coursesRes.json();
+            setCourses(courses);
+        } else {
+            setCourses([]);
+        }
+    }, []);
 
-	return (
-		<StudentProtection setLoggedInStudent={setLoggedInStudent}>
-			<div className={styles.container}>
-				<Head>
-					<title>
-						Student Dashboard | Automating Elective Processing
-					</title>
-					<link rel="icon" href="/favicon.ico" />
-				</Head>
+    return (
+        <StudentProtection setLoggedInStudent={setLoggedInStudent}>
+            <div className={styles.container}>
+                <Head>
+                    <title>
+                        Student Dashboard | Automating Elective Processing
+                    </title>
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
 
-				<header className={styles.header}>
-					<div className={styles.headingLeft}>
-						<Image
-							src="/amritaLogo.png"
-							alt="Amrita Logo"
-							className={styles.logo}
-							width={300}
-							height={100}
-						/>
-						<h2 className={styles.pageTitle}>Student Dashboard</h2>
-					</div>
-					<div className={styles.headingRight}>
-						<nav
-							style={{
-								display: "flex",
-								flexWrap: "wrap",
-							}}
-						>
-							<Link href={"/student/dashboard"}>
-								<a className={styles.navLink}>Home</a>
-							</Link>
-							<Link href={"/student/viewFaculties"}>
-								<a className={styles.navLink}>View Faculties</a>
-							</Link>
-							<Link href={"/student/status/round1"}>
-								<a className={styles.navLink}>Round 1 Status</a>
-							</Link>
-							<Link href={"/student/status/round2"}>
-								<a className={styles.navLink}>Round 2 Status</a>
-							</Link>
-						</nav>
-					</div>
-				</header>
-				<main className={styles.main}>
-					{/* Props will have to obtained from Auth service */}
-					<ControlBar user={{ name: loggedInStudent?.name }} />
+                <header className={styles.header}>
+                    <div className={styles.headingLeft}>
+                        <Image
+                            src="/amritaLogo.png"
+                            alt="Amrita Logo"
+                            className={styles.logo}
+                            width={300}
+                            height={100}
+                        />
+                        <h2 className={styles.pageTitle}>Student Dashboard</h2>
+                    </div>
+                    <div className={styles.headingRight}>
+                        <nav
+                            style={{
+                                display: "flex",
+                                flexWrap: "wrap",
+                            }}
+                        >
+                            <Link href={"/student/dashboard"}>
+                                <a className={styles.navLink}>Home</a>
+                            </Link>
+                            <Link href={"/student/viewFaculties"}>
+                                <a className={styles.navLink}>View Faculties</a>
+                            </Link>
+                            <Link href={"/student/status/round1"}>
+                                <a className={styles.navLink}>Round 1 Status</a>
+                            </Link>
+                            <Link href={"/student/status/round2"}>
+                                <a className={styles.navLink}>Round 2 Status</a>
+                            </Link>
+                        </nav>
+                    </div>
+                </header>
+                <main className={styles.main}>
+                    {/* Props will have to obtained from Auth service */}
+                    <ControlBar user={{ name: loggedInStudent?.name }} />
 
-					{/* Table of courses offered */}
-					{courses?.length > 0 ? (
-						<CourseTable
-							courses={courses}
-							setCourses={setCourses}
-						/>
-					) : (
-						"No Courses on Database"
-					)}
-				</main>
+                    {/* Table of courses offered */}
+                    {courses?.length > 0 ? (
+                        <CourseTable
+                            courses={courses}
+                            setCourses={setCourses}
+                            user={loggedInStudent}
+                        />
+                    ) : (
+                        "No Courses on Database"
+                    )}
+                </main>
 
-				<footer className={styles.footer}>
-					<div>Automating Elective Processing</div>
-					<div>CSE-F</div>
-				</footer>
-			</div>
-		</StudentProtection>
-	);
+                <footer className={styles.footer}>
+                    <div>Automating Elective Processing</div>
+                    <div>CSE-F</div>
+                </footer>
+            </div>
+        </StudentProtection>
+    );
 }
--- frontend/src/components/student/dashboard/CourseTable/index.js
+++ frontend/src/components/student/dashboard/CourseTable/index.js
@@ -5,113 +5,156 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 const CourseTable = (props) => {
-	const [eligibleCourses, setEligibleCourses] = useState([]);
-	const [ineligibleCourses, setIneligibleCourses] = useState([]);
+    const [eligibleCourses, setEligibleCourses] = useState([]);
+    const [ineligibleCourses, setIneligibleCourses] = useState([]);
+    useEffect(() => {
+        setEligibleCourses(
+            props.courses.filter((course) => {
+                return course.minCGPA <= props.user.cgpa;
+            })
+        );
+        setIneligibleCourses(
+            props.courses.filter((course) => {
+                return course.minCGPA > props.user.cgpa;
+            })
+        );
+    }, []);
 
-	useEffect(() => {
-		setEligibleCourses(props.courses.filter((course) => {}));
-	}, []);
+    const pushCourseUp = (currIdx) => {
+        if (currIdx != 0) {
+            let newPrefs = Array.from(eligibleCourses);
+            let temp = newPrefs[currIdx];
+            newPrefs[currIdx] = newPrefs[currIdx - 1];
+            newPrefs[currIdx - 1] = temp;
+            setEligibleCourses(newPrefs);
+        }
+    };
+    const pushCourseDown = (currIdx) => {
+        if (currIdx < eligibleCourses.length - 1) {
+            let newPrefs = Array.from(eligibleCourses);
+            let temp = newPrefs[currIdx];
+            newPrefs[currIdx] = newPrefs[currIdx + 1];
+            newPrefs[currIdx + 1] = temp;
+            setEligibleCourses(newPrefs);
+        }
+    };
+    const router = useRouter();
 
-	const pushCourseUp = (currIdx) => {
-		if (currIdx != 0) {
-			let newPrefs = Array.from(props.courses);
-			let temp = newPrefs[currIdx];
-			newPrefs[currIdx] = newPrefs[currIdx - 1];
-			newPrefs[currIdx - 1] = temp;
-			props.setCourses(newPrefs);
-		}
-	};
-	const pushCourseDown = (currIdx) => {
-		if (currIdx < props.courses.length - 1) {
-			let newPrefs = Array.from(props.courses);
-			let temp = newPrefs[currIdx];
-			newPrefs[currIdx] = newPrefs[currIdx + 1];
-			newPrefs[currIdx + 1] = temp;
-			props.setCourses(newPrefs);
-		}
-	};
-	const router = useRouter();
-
-	return (
-		<div className={styles.container}>
-			<table cellSpacing={0}>
-				<thead>
-					<tr>
-						<th className={styles.snoCell}>S.no</th>
-						<th className={styles.courseCodeCell}>Course Code</th>
-						<th className={styles.courseNameCell}>Course Name</th>
-						<th className={styles.departmentCell}>Department</th>
-						<th className={styles.addedByCell}>Added By</th>
-						<th className={styles.addedOnCell}>Added On</th>
-						<th className={styles.minCapCell}>Min.Cap</th>
-						<th className={styles.maxCapCell}>Max.Cap</th>
-						<th className={styles.reorderCell}>Reorder</th>
-					</tr>
-				</thead>
-				<tbody>
-					{props.courses.map((course, idx) => (
-						<tr
-							key={idx}
-							data-id={course._id}
-							title={`Pre-requisites are ${course.preRequisites}`}
-						>
-							<td className={styles.snoCellData}>{idx + 1}</td>
-							<td className={styles.courseCodeCellData}>
-								{course.courseCode}
-							</td>
-							<td className={styles.courseNameCellData}>
-								{course.courseName}
-								<br /> (Credits: {course.credits} - Min CGPA:{" "}
-								{course.minCGPA})
-							</td>
-							<td className={styles.departmentCellData}>
-								{course.department}
-							</td>
-							<td className={styles.addedByCellData}>
-								{course.addedBy}
-							</td>
-							<td className={styles.addedOnCellData}>
-								{new Date(course.addedOn).toLocaleDateString()}
-							</td>
-							<td className={styles.minCapCellData}>
-								{course.minCap}
-							</td>
-							<td className={styles.maxCapCellData}>
-								{course.maxCap}
-							</td>
-							<td className={styles.reorderCellData}>
-								{idx != 0 ? (
-									<FaChevronUp
-										style={{
-											marginRight: 10,
-											fontSize: "26px",
-											cursor: "pointer",
-										}}
-										onClick={() => pushCourseUp(idx)}
-									/>
-								) : (
-									<></>
-								)}
-								{idx != props.courses.length - 1 ? (
-									<FaChevronDown
-										style={{
-											marginLeft: 10,
-											fontSize: "26px",
-											cursor: "pointer",
-										}}
-										onClick={() => pushCourseDown(idx)}
-									/>
-								) : (
-									<></>
-								)}
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-			<FreezeBtn />
-		</div>
-	);
+    return (
+        <div className={styles.container}>
+            <table cellSpacing={0}>
+                <thead>
+                    <tr>
+                        <th className={styles.snoCell}>S.no</th>
+                        <th className={styles.courseCodeCell}>Course Code</th>
+                        <th className={styles.courseNameCell}>Course Name</th>
+                        <th className={styles.departmentCell}>Department</th>
+                        <th className={styles.addedByCell}>Added By</th>
+                        <th className={styles.addedOnCell}>Added On</th>
+                        <th className={styles.minCapCell}>Min.Cap</th>
+                        <th className={styles.maxCapCell}>Max.Cap</th>
+                        <th className={styles.reorderCell}>Reorder</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {eligibleCourses.map((course, idx) => (
+                        <tr
+                            key={idx}
+                            data-id={course._id}
+                            title={`Pre-requisites are ${course.preRequisites}`}
+                        >
+                            <td className={styles.snoCellData}>{idx + 1}</td>
+                            <td className={styles.courseCodeCellData}>
+                                {course.courseCode}
+                            </td>
+                            <td className={styles.courseNameCellData}>
+                                {course.courseName}
+                                <br /> (Credits: {course.credits} - Min CGPA:{" "}
+                                {course.minCGPA})
+                            </td>
+                            <td className={styles.departmentCellData}>
+                                {course.department}
+                            </td>
+                            <td className={styles.addedByCellData}>
+                                {course.addedBy}
+                            </td>
+                            <td className={styles.addedOnCellData}>
+                                {new Date(course.addedOn).toLocaleDateString()}
+                            </td>
+                            <td className={styles.minCapCellData}>
+                                {course.minCap}
+                            </td>
+                            <td className={styles.maxCapCellData}>
+                                {course.maxCap}
+                            </td>
+                            <td className={styles.reorderCellData}>
+                                {idx != 0 ? (
+                                    <FaChevronUp
+                                        style={{
+                                            marginRight: 10,
+                                            fontSize: "26px",
+                                            cursor: "pointer",
+                                        }}
+                                        onClick={() => pushCourseUp(idx)}
+                                    />
+                                ) : (
+                                    <></>
+                                )}
+                                {idx != eligibleCourses.length - 1 ? (
+                                    <FaChevronDown
+                                        style={{
+                                            marginLeft: 10,
+                                            fontSize: "26px",
+                                            cursor: "pointer",
+                                        }}
+                                        onClick={() => pushCourseDown(idx)}
+                                    />
+                                ) : (
+                                    <></>
+                                )}
+                            </td>
+                        </tr>
+                    ))}
+                    {/* Show inEligible courses in different color scheme */}
+                    {ineligibleCourses.map((course, idx) => (
+                        <tr
+                            key={idx}
+                            data-id={course._id}
+                            className={styles.ineligibleCourseRow}
+                            title={`Pre-requisites are ${course.preRequisites} | Minimum CGPA not attained`}
+                        >
+                            <td className={styles.snoCellData}>-</td>
+                            <td className={styles.courseCodeCellData}>
+                                {course.courseCode}
+                            </td>
+                            <td className={styles.courseNameCellData}>
+                                {course.courseName}
+                                <br /> (Credits: {course.credits} - Min CGPA:{" "}
+                                {course.minCGPA})
+                            </td>
+                            <td className={styles.departmentCellData}>
+                                {course.department}
+                            </td>
+                            <td className={styles.addedByCellData}>
+                                {course.addedBy}
+                            </td>
+                            <td className={styles.addedOnCellData}>
+                                {new Date(course.addedOn).toLocaleDateString()}
+                            </td>
+                            <td className={styles.minCapCellData}>
+                                {course.minCap}
+                            </td>
+                            <td className={styles.maxCapCellData}>
+                                {course.maxCap}
+                            </td>
+                            <td className={styles.reorderCellData}>-</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <FreezeBtn />
+        </div>
+    );
 };
 
 export default CourseTable;
