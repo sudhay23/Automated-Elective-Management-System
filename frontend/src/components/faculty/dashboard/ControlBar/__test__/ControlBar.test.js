191d4b62f509976cbcc7ec719242970f17749df8
sudhay23 <sudhay2001@gmail.com>
2022-05-14 22:59:08 +0530

    Unit test commented for sample

--- frontend/src/components/faculty/dashboard/ControlBar/__test__/ControlBar.test.js
+++ frontend/src/components/faculty/dashboard/ControlBar/__test__/ControlBar.test.js
@@ -1,13 +1,15 @@
-import ControlBar from "../index";
+import ControlBar from "../index.js";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
 describe("Control Bar", () => {
+	// Unit Test 1
 	test("Checks if Add Course Button is present", () => {
 		render(<ControlBar />);
 		expect(screen.getByText("Add New Course")).toBeInTheDocument();
 	});
 
+	// Unit Test 2
 	test("Checks if User's name is being greeted", () => {
 		render(<ControlBar user={{ name: "Sudhay" }} />);
 		expect(screen.getByTestId("user-greeting").textContent).toEqual(
